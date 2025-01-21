bb02422119be35a2f2bb1c4ddfa12a57721b6443
dimitarmitkov <dimitar.mitkov@me.com>
2025-01-21 16:35:21 +0200

    v23 add changes to iter and maps

--- iterAll.go
+++ iterAll.go
@@ -3,62 +3,79 @@ package main
 import (
 	"fmt"
 	"iter"
+	"reflect"
 	"slices"
 )
 
+// AllFn Slices All
 func AllFn() {
 	slice := []int{1, 2, 3}
 	it := slices.All(slice)
+	fmt.Println("type of All: ", reflect.TypeOf(it))
 	for i, itr := range it {
 		fmt.Printf("Index: %d, Value: %d\n", i, itr)
 	}
 }
 
+// AppSeq Values and AppendSeq
 func AppSeq() {
 	existing := []int{1, 2}
 	it := slices.Values([]int{3, 4})
+	fmt.Println("type of Values", reflect.TypeOf(it))
 	combined := slices.AppendSeq(existing, it)
+	fmt.Println("type of AppendSeq", reflect.TypeOf(it))
 	fmt.Println(combined)
 }
 
+// Coll Collect
 func Coll() {
 	it := slices.Values([]int{1, 2, 3})
 	newSlice := slices.Collect(it)
+	fmt.Println("type of Collect: ", reflect.TypeOf(it))
 	fmt.Println(newSlice)
 }
 
+// Bckwd Backward
 func Bckwd() {
 	slice := []int{1, 2, 3}
 	it := slices.Backward(slice)
+	fmt.Println("type of Backward: ", reflect.TypeOf(it))
 	for i, i2 := range it {
 		fmt.Println(i, i2)
 	}
 }
 
+// Srt Sorted
 func Srt() {
 	slice := []int{5, 6, 7, 1, 2, 3}
+	fmt.Println("type of slice: ", reflect.TypeOf(slice))
 	it := slices.Values(slice)
 	sort := slices.Sorted(it)
+	fmt.Println("type of Sorted: ", reflect.TypeOf(it))
 	fmt.Println(sort)
 }
 
+// Chnk Chunk
 func Chnk() {
 	slice := []int{1, 2, 3, 4, 5}
 	it := slices.Chunk(slice, 3)
+	fmt.Println("type of Chunk: ", reflect.TypeOf(it))
 	slicePrint("chunk ", it)
-}
-
-func Val() {
-	slice := []int{1, 2, 3}
-	it := slices.Values(slice)
-
-	for x := range it {
-		fmt.Println("val ", x)
-	}
 }
 
 func slicePrint(keyWord string, it iter.Seq[[]int]) {
 	for x := range it {
 		fmt.Println(keyWord, x)
+	}
+}
+
+// Val Values
+func Val() {
+	slice := []int{1, 2, 3}
+	it := slices.Values(slice)
+	fmt.Println("type of Values: ", reflect.TypeOf(it))
+
+	for x := range it {
+		fmt.Println("val ", x)
 	}
 }
--- maps.go
+++ maps.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"maps"
+	"reflect"
 )
 
 func AllMaps() {
 	m := map[string]int{"one": 1, "two": 2}
 	it := maps.All(m)
 
+	fmt.Println("type of map All:", reflect.TypeOf(it))
 	for k, v := range it {
 		fmt.Printf("Key: %s, Value: %d\n", k, v)
 	}
@@ -17,6 +19,7 @@ func AllMaps() {
 func KeysMap() {
 	m := map[string]int{"one": 1, "two": 2}
 	it := maps.Keys(m)
+	fmt.Println("type of map Keys:", reflect.TypeOf(it))
 	for k := range it {
 		fmt.Printf("Key: %s\n", k)
 	}
@@ -26,6 +29,7 @@ func KeysMap() {
 func ValuesMap() {
 	m := map[string]int{"one": 1, "two": 2}
 	it := maps.Values(m)
+	fmt.Println("type of map Values:", reflect.TypeOf(it))
 	for v := range it {
 		fmt.Printf("Value: %d\n", v)
 	}
@@ -35,11 +39,13 @@ func InsertMap() {
 	m := map[string]int{"one": 1}
 	it := maps.All(map[string]int{"two": 2, "three": 3})
 	maps.Insert(m, it)
+	fmt.Println("type of map with insert:", reflect.TypeOf(m))
 	fmt.Println(m)
 }
 
 func CollectMap() {
 	it := maps.All(map[string]int{"one": 1, "two": 2})
 	newMap := maps.Collect(it)
+	fmt.Println("type of collected maps:", reflect.TypeOf(newMap))
 	fmt.Println(newMap)
 }
